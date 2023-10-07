fd0f4ae57452fdcdba318f769a838ba535e4f8cb
Shadabdullah <shadabdullah101@gmail.com>
2023-10-07 09:37:01 +0530

    Add ColorScheme

--- wallpy/lib/main.dart
+++ wallpy/lib/main.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/material.dart';
 
+final colorScheme = ColorScheme.fromSeed(
+    seedColor: const Color.fromARGB(128, 64, 23, 53), );
+
+final theme = ThemeData()
+    .copyWith(useMaterial3: true, scaffoldBackgroundColor: colorScheme.primary);
+
 void main() {
   runApp(const MyApp());
 }
