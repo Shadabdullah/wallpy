0259abe00cd4fdfe281ad24eab5c2c0c53039126
Shadabdullah <shadabdullah101@gmail.com>
2023-10-08 12:10:02 +0530

    App bar Title

--- wallpy/lib/main.dart
+++ wallpy/lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 final colorScheme = ColorScheme.fromSeed(
-    seedColor: const Color.fromARGB(128, 64, 23, 53), );
+  seedColor: const Color.fromARGB(128, 64, 23, 53),
+  primary: const Color.fromARGB(128, 64, 23, 53),
+);
 
-final theme = ThemeData()
-    .copyWith(useMaterial3: true, scaffoldBackgroundColor: colorScheme.primary);
+final theme = ThemeData().copyWith(
+    useMaterial3: true,
+    scaffoldBackgroundColor: colorScheme.primary,
+    textTheme: GoogleFonts.ubuntuCondensedTextTheme().copyWith(
+        titleSmall: GoogleFonts.ubuntuCondensed(fontWeight: FontWeight.bold),
+        titleMedium: GoogleFonts.ubuntuCondensed(fontStyle: FontStyle.normal)));
 
 void main() {
   runApp(const MyApp());
@@ -18,7 +25,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text("Wallpy")),
+        appBar: AppBar(
+          title:
+              Text("Wallpy", style: Theme.of(context).textTheme.titleMedium!),
+        ),
       ),
     );
   }
